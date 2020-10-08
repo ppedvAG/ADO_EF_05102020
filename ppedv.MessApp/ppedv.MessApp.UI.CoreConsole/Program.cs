492190c5df1adcc9e93033f2c279ccecfe6a6c92
André <ruland@outlook.de>
2020-10-08 15:18:40 +0200

    UI mit Core Console

---
+++ ppedv.MessApp/ppedv.MessApp.UI.CoreConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using ppedv.MessApp.Logic;
+using ppedv.MessApp.Model;
+using System;
+
+namespace ppedv.MessApp.UI.CoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var core = new Core(new Data.EF.EfRepository());
+
+            Console.WriteLine($"Messungen heute: {core.CountMessungOfDay(DateTime.Now)}");
+
+            foreach (var ml in core.Repository.GetAll<Messlauf>())
+            {
+                Console.WriteLine($"{ml.GestartetVon} {ml.Start} {ml.GemessenesGerät}");
+            }
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
