dc7549eb3536018d6118d628345eefeb9e005f71
André <ruland@outlook.de>
2020-10-08 14:45:18 +0200

    Repository und simple BL fertig

---
+++ ppedv.MessApp/ppedv.MessApp.Data.EF/EfRepository.cs
@@ -0,0 +1,49 @@
+﻿using ppedv.MessApp.Model;
+using ppedv.MessApp.Model.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.MessApp.Data.EF
+{
+    public class EfRepository : IRepository
+    {
+        EfContext con = new EfContext();
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            con.Set<T>().Add(entity);
+            //if (typeof(T) == typeof(Messlauf))
+            //    con.Messläufe.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            con.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return con.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : Entity
+        {
+            return con.Set<T>().Find(id);
+        }
+
+        public int SaveAll()
+        {
+            return con.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            var loaded = GetById<T>(entity.Id);
+            if (loaded != null)
+                con.Entry(loaded).CurrentValues.SetValues(entity);
+        }
+    }
+}
---
+++ ppedv.MessApp/ppedv.MessApp.Logic/Core.cs
@@ -0,0 +1,23 @@
+﻿using ppedv.MessApp.Model;
+using ppedv.MessApp.Model.Contracts;
+using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ppedv.MessApp.Logic
+{
+    public class Core
+    {
+        public IRepository Repository { get; private set; }
+
+        public Core(IRepository repo)
+        {
+            Repository = repo;
+        }
+
+        public int CountMessungOfDay(DateTime day)
+        {
+            return Repository.GetAll<Messung>().Count(x => x.MessZeit.Date == day.Date);
+        }
+    }
+}
