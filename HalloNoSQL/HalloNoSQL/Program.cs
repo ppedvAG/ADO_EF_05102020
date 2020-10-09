3ba518f8b2a8c98e79cba0679afa2225be347171
André <ruland@outlook.de>
2020-10-09 16:33:13 +0200

    noSQL

---
+++ HalloNoSQL/HalloNoSQL/Program.cs
@@ -0,0 +1,38 @@
+﻿using LiteDB;
+using System;
+
+namespace HalloNoSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var db = new LiteDatabase(@"db.db"))
+            {
+                var col = db.GetCollection<Auto>("Autos");
+
+                col.Insert(new Auto() { Hersteller = "Bercedes", KW = 2000 });
+
+                foreach (var a in col.Query().ToList())
+                {
+                    Console.WriteLine($"{a.Id} {a.Hersteller}");
+                }
+
+
+            }
+
+
+        }
+    }
+
+    public class Auto
+    {
+        public int Id { get; set; }
+        public string Hersteller { get; set; }
+
+        public int KW { get; set; }
+
+    }
+}
