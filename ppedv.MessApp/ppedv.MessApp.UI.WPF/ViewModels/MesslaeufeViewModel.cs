5626a1eaa8ff38747567887365c827be8f6b0b2e
André <ruland@outlook.de>
2020-10-09 15:13:48 +0200

    Laden, Speichern, löschen funktioniert

--- ppedv.MessApp/ppedv.MessApp.UI.WPF/ViewModels/MesslaeufeViewModel.cs
+++ ppedv.MessApp/ppedv.MessApp.UI.WPF/ViewModels/MesslaeufeViewModel.cs
@@ -2,9 +2,11 @@
 using ppedv.MessApp.Logic;
 using ppedv.MessApp.Model;
 using ppedv.MessApp.Model.Contracts;
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Input;
 
 namespace ppedv.MessApp.UI.WPF.ViewModels
 {
@@ -15,6 +17,10 @@ namespace ppedv.MessApp.UI.WPF.ViewModels
         public ObservableCollection<Messlauf> MesslaufListe { get; set; }
 
         public Messlauf SelectedMesslauf { get; set; }
+
+        public ICommand SaveCommand { get; set; }
+        public ICommand NewCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
 
 
         public MesslaeufeViewModel() : this(null)
@@ -29,7 +35,23 @@ namespace ppedv.MessApp.UI.WPF.ViewModels
                 _core = core;
 
             MesslaufListe = new ObservableCollection<Messlauf>(_core.UnitOfWork.MesslaufRepository.Query().ToList());
+            SaveCommand = new RelayCommand(x => _core.UnitOfWork.SaveAll());
+            DeleteCommand = new RelayCommand(x => 
+            {
+                if (SelectedMesslauf == null)
+                    return;
+                _core.UnitOfWork.GetRepo<Messlauf>().Delete(SelectedMesslauf);
+                MesslaufListe.Remove(SelectedMesslauf);
+            });
+            NewCommand = new RelayCommand(CreateNewMesslauf);
+        }
 
+        private void CreateNewMesslauf(object obj)
+        {
+            var ml = new Messlauf() { GemessenesGerät = "NEU", GestartetVon = "Peter", Start = DateTime.Now };
+            _core.UnitOfWork.MesslaufRepository.Add(ml);
+            MesslaufListe.Add(ml);
+            SelectedMesslauf = ml;
         }
     }
 }
